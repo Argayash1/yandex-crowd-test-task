b70afb3a8d419a301ac0c70026743d70cfffda80
Argayash <zilberman.yakoff@yandex.ru>
2024-05-30 16:10:40 +0300

    feat: сверстать секцию participants

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,10 @@
 import stagesCards from "./stagesCards.js";
+import { participants } from "./participants.js";
 
 const leadSectionElement = document.querySelector(".lead");
 const participantsSectionElement = document.querySelector(".participants");
 const stagesListElement = document.querySelector(".stages__list");
+const participantListElement = document.querySelector(".participants__list");
 
 const elementTemplate = document
   .querySelector("#running-line-template")
@@ -12,14 +14,13 @@ const stageCardTemplate = document
   .querySelector("#stage-card-template")
   .content.querySelector(".stage-card");
 
+const participantCardTemplate = document
+  .querySelector("#participant-card-template")
+  .content.querySelector(".participant-card");
+
 const createElement = () => {
   const elementElement = elementTemplate.cloneNode(true);
   return elementElement;
-};
-
-const renderElement = (wrapElement, createElementFunction, item = null) => {
-  const element = item ? createElementFunction(item) : createElementFunction();
-  wrapElement.append(element);
 };
 
 const createStageCard = (item) => {
@@ -35,9 +36,33 @@ const createStageCard = (item) => {
   return stageCardElement;
 };
 
+const createParticipantCard = (item) => {
+  const participantCardElement = participantCardTemplate.cloneNode(true);
+  const participantCardElementTitle = participantCardElement.querySelector(
+    ".participant-card__title"
+  );
+  const participantCardElementSubtitle = participantCardElement.querySelector(
+    ".participant-card__subtitle"
+  );
+
+  participantCardElementTitle.textContent = item.name;
+  participantCardElementSubtitle.textContent = item.title;
+
+  return participantCardElement;
+};
+
+const renderElement = (wrapElement, createElementFunction, item = null) => {
+  const element = item ? createElementFunction(item) : createElementFunction();
+  wrapElement.append(element);
+};
+
 renderElement(leadSectionElement, createElement);
 renderElement(participantsSectionElement, createElement);
 
 stagesCards.forEach((item) => {
   renderElement(stagesListElement, createStageCard, item);
+});
+
+participants.forEach((item) => {
+  renderElement(participantListElement, createParticipantCard, item);
 });
---
+++ scripts/participants.js
@@ -0,0 +1,13 @@
+const worldChessChampion = "Чемпион мира по шахматам";
+const grossMeister = "Гроссмейстер";
+
+const participants = [
+  { name: "Хозе-Рауль Капабланка", title: worldChessChampion },
+  { name: "Эммануил Ласкер", title: worldChessChampion },
+  { name: "Александр Алехин", title: worldChessChampion },
+  { name: "Арон Нимцович", title: worldChessChampion },
+  { name: "Рихард Рети", title: worldChessChampion },
+  { name: "Остап Бендер", title: grossMeister },
+];
+
+export { participants };
