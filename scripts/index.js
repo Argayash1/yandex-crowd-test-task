7dd2d4ab1c2d44776ec0b6ca38ff4f278b079f95
Argayash <zilberman.yakoff@yandex.ru>
2024-06-01 23:19:24 +0300

    feat: написать логику работы слайдера в секции participants

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,21 @@ import { participants } from "./participants.js";
 const leadSectionElement = document.querySelector(".lead");
 const participantsSectionElement = document.querySelector(".participants");
 const stagesListElement = document.querySelector(".stages__list");
-const participantListElement = document.querySelector(".participants__list");
+const participantListElement = participantsSectionElement.querySelector(
+  ".participants__list"
+);
+const participantsSlideNumber = participantsSectionElement.querySelector(
+  ".participants__slide-number"
+);
+const participantsCount = participantsSectionElement.querySelector(
+  ".participants__slide-number_type_all"
+);
+const participantsPrevButton = participantsSectionElement.querySelector(
+  ".participants__button"
+);
+const participantsNextButton = participantsSectionElement.querySelector(
+  ".participants__button_type_next"
+);
 
 const elementTemplate = document
   .querySelector("#running-line-template")
@@ -17,6 +31,9 @@ const stageCardTemplate = document
 const participantCardTemplate = document
   .querySelector("#participant-card-template")
   .content.querySelector(".participant-card");
+
+let stagesSliderCount = 0;
+let participantsSliderCount = 0;
 
 const createElement = () => {
   const elementElement = elementTemplate.cloneNode(true);
@@ -51,9 +68,72 @@ const createParticipantCard = (item) => {
   return participantCardElement;
 };
 
+const setParticipantsSlideNumber = (slideNumber) => {
+  participantsSlideNumber.textContent = slideNumber;
+};
+
+const setParticipantsCount = () => {
+  participantsCount.textContent = `/ ${participants.length}`;
+};
+
 const renderElement = (wrapElement, createElementFunction, item = null) => {
   const element = item ? createElementFunction(item) : createElementFunction();
   wrapElement.append(element);
+};
+
+const rollSlider = (slidesList, offset, sliderCount) => {
+  slidesList.style.transform = `translateX(-${sliderCount * offset}px)`;
+};
+
+const changeToNextSlide = (slidesList, sliderCount, sliderButtons) => {
+  const slides = slidesList.querySelectorAll("li");
+  const offset =
+    slides[0].offsetWidth +
+    +window
+      .getComputedStyle(slidesList)
+      .getPropertyValue("gap")
+      .replace("px", "");
+
+  sliderCount++;
+  sliderButtons.prevButton.disabled = false;
+
+  if (sliderCount > slides.length - 3) {
+    sliderCount = 0;
+    sliderButtons.prevButton.disabled = true;
+  }
+
+  rollSlider(slidesList, offset, sliderCount);
+
+  if (slidesList.classList.contains("participants__list")) {
+    participantsSlideNumber.textContent = sliderCount + 3;
+
+    sliderButtons.nextButton.disabled =
+      sliderCount === slides.length - 3 ? true : false;
+  }
+
+  return sliderCount;
+};
+
+const changeToPrevSlide = (slidesList, sliderCount, sliderPrevButton) => {
+  const slides = slidesList.querySelectorAll("li");
+  const offset =
+    slides[0].offsetWidth +
+    +window
+      .getComputedStyle(slidesList)
+      .getPropertyValue("gap")
+      .replace("px", "");
+
+  sliderCount--;
+
+  sliderPrevButton.disabled = sliderCount === 0 ? true : false;
+
+  rollSlider(slidesList, offset, sliderCount);
+
+  if (slidesList.classList.contains("participants__list")) {
+    participantsSlideNumber.textContent = sliderCount + 3;
+  }
+
+  return sliderCount;
 };
 
 renderElement(leadSectionElement, createElement);
@@ -65,4 +145,31 @@ stagesCards.forEach((item) => {
 
 participants.forEach((item) => {
   renderElement(participantListElement, createParticipantCard, item);
+});
+
+setParticipantsCount();
+setParticipantsSlideNumber(participants.length - 3);
+
+setInterval(() => {
+  participantsSliderCount = changeToNextSlide(
+    participantListElement,
+    participantsSliderCount,
+    { nextButton: participantsNextButton, prevButton: participantsPrevButton }
+  );
+}, 4000);
+
+participantsPrevButton.addEventListener("click", () => {
+  participantsSliderCount = changeToPrevSlide(
+    participantListElement,
+    participantsSliderCount,
+    participantsPrevButton
+  );
+});
+
+participantsNextButton.addEventListener("click", () => {
+  participantsSliderCount = changeToNextSlide(
+    participantListElement,
+    participantsSliderCount,
+    { nextButton: participantsNextButton, prevButton: participantsPrevButton }
+  );
 });
